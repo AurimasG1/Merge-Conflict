65703e258bcd5c395609ff758ade253112d769c6
AurimasG1 <112963534+AurimasG1@users.noreply.github.com>
2026-02-01 21:26:46 +0200

    Restore task.js

---
+++ task.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const openMenu = document.getElementById("openMenu");
+const closeMenu = document.getElementById("closeMenu");
+const mobileMenu = document.getElementById("mobileMenu");
+
+openMenu.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
+
+ 
